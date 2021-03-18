be2876d22ff1499355f07236130a5418036ddcc0
david <dh@ptica.com>
2021-03-18 15:22:23 +0100

    fixed images bug -> moved them back to src and imported them with require + function

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import images from '../imageGetter';
 
 function Card(props) {
     return(
         <div name={props.name} onClick={props.checkAnswer} className="card">
-            <img src={`/images/${props.name}.png`} name={props.name}/>
+            <img src={images[`${props.name}.png`].default} name={props.name}/>
             <p name={props.name}>{props.name}</p>
         </div>
     );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import githubImg from '../images/github.png';
 
 function Footer() {
     return (
         <footer className='footer'>
             <p className='footer-text'>Zirafnik</p>
-            <a target='_blank' href='https://github.com/Zirafnik/memory-card-game'><img className='github-image' src={`/images/github.png`}/></a>
+            <a target='_blank' href='https://github.com/Zirafnik/memory-card-game'><img className='github-image' src={githubImg} /></a>
         </footer>
     )
 }
